bacc738bb4f47518b5f0f34baf84fb3642ddc236
Aseel <Aseel.Bahaziq@gmail.com>
2018-02-22 06:00:10 +0300

    Added the iOSExercise project to git repository.

---
+++ iOSExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSExercise
+//
+//  Created by Aseel Bahaziq on 2/22/18.
+//  Copyright © 2018 Aseel Bahaziq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
